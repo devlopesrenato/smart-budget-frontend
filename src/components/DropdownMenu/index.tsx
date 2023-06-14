ed56fecfb39906ce176c0b4750c31e92b92fa492
devLopesRenato <renatolopes37@gmail.com>
2023-06-14 01:39:20 -0300

    feat: Ajustes no header - menu usuário

---
+++ src/components/DropdownMenu/index.tsx
@@ -0,0 +1,52 @@
+import { Dropdown, Menu } from "antd";
+import { ReactNode } from "react";
+
+interface ItemType {
+    label: string,
+    key: string | number,
+    icon?: ReactNode,
+    action?: () => void
+}
+
+type PlacementType = "bottomRight" | "topLeft" | "topCenter" | "topRight" | "bottomLeft" | "bottomCenter" | "top" | "bottom" | undefined
+
+type Props = {
+    children: React.ReactNode;
+    items: ItemType[];
+    placement?: PlacementType
+};
+
+export const DropdownMenu: React.FC<Props> = ({ children, items = [], placement = 'bottomRight' }) => {
+
+    const menu = (
+        <Menu
+            style={{
+                backgroundColor: '#2e3034',
+                borderRadius: 'var(--border-radius)',
+                transition: 'background 200ms, border 200ms',
+                background: 'rgba(78, 80, 88, 0.91)',
+                border: '1px solid rgba(var(--card-border-rgb), 0.15)',
+                color: 'white',
+                textShadow: '0px 2px 4px rgba(0, 0, 0, 0.3)',
+            }}
+        >
+            {items.map(item =>
+                <Menu.Item
+                    style={{ color: 'white' }}
+                    icon={item.icon}
+                    key={item.key}
+                    onClick={item.action}
+                >
+                    {item.label}
+                </Menu.Item>
+            )}
+        </Menu>
+    );
+
+
+    return (
+        <Dropdown dropdownRender={() => menu} placement={placement}>
+            {children}
+        </Dropdown>
+    )
+}
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,13 @@
 import { AppContext } from '@/context/app.context';
 import { Utils } from '@/utils/utils';
+import { SolutionOutlined, UserOutlined } from '@ant-design/icons';
+import { Avatar } from 'antd';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import { useContext, useEffect, useState } from 'react';
-import { FiHome, FiLogOut } from 'react-icons/fi';
+import { FiHome } from 'react-icons/fi';
+import { HiOutlineLogout } from 'react-icons/hi';
+import { DropdownMenu } from '../DropdownMenu';
 import styles from './header.module.css';
 
 const _ = new Utils()
@@ -22,11 +26,13 @@ export const Header: React.FC = () => {
 
     const pagesWithHeader: string[] = [
         'sheet',
+        'details'
     ]
 
     const title = {
         'none': '',
-        'sheet': 'Folhas de Orçamento'
+        'sheet': 'Folhas de Orçamento',
+        'details': 'Detalhes',
     }
 
     function getTitle(page: string) {
@@ -54,6 +60,20 @@ export const Header: React.FC = () => {
         _.removeCookie('userData')
     }
 
+    const itemsMenu = [
+        {
+            label: 'Minha Conta',
+            key: 'account',
+            icon: <SolutionOutlined style={{ color: '#00C5FF' }} size={18} />
+        },
+        {
+            label: 'Sair',
+            key: 'logout',
+            icon: <HiOutlineLogout color='#D94848' size={18} />,
+            action: () => logout()
+        }
+    ]
+
     return (
         (user !== null && !loading) ?
             (
@@ -65,13 +85,12 @@ export const Header: React.FC = () => {
                 >
                     <p>{getTitle(page)}</p>
                     <div className={styles.userSection}>
-                        <p>{user.name}</p>
-                        <FiLogOut
-                            className={styles.iconLogout}
-                            onClick={() =>
-                                logout()
-                            }
-                        />
+                        <DropdownMenu
+                            items={itemsMenu}
+                            placement={'bottomLeft'}
+                        >
+                            <Avatar style={{ backgroundColor: '#332d2d' }} icon={<UserOutlined />} />
+                        </DropdownMenu>
                     </div>
                 </div>
             ) : (
